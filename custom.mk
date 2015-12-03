c5b05391c7cdc6d00143c78c3d61e19677d56200
buidanhquy <quy.buidanhquy@gmail.com>
2015-12-03 08:50:34 +0700

    update

--- custom.mk
+++ custom.mk
@@ -1,4 +1,4 @@
-CROSS_COMPILE:=1
+CROSS_COMPILE:=2
 CROSS_ARM:=1
 CROSS_LINUX_X86:=2
 CROSS_MINGW_X86:=3
@@ -6,7 +6,7 @@ CROSS_MACOS_X86_64:=4
 
 ##### FOR ARM ####################
 ifeq ($(CROSS_COMPILE),$(CROSS_ARM))
-	CROSS_TOOL:=arm-linux-gnueabi-gcc
+	CROSS_TOOL:=arm-none-linux-gnueabi-gcc
 	LIBS_DIR:=../libs/linux-armv7l
 	LIBS:= -L$(LIBS_DIR)/lib $(LIBS_DIR)/lib/libjson-c.a -lpjsua2-arm-none-linux-gnueabi -lstdc++ -lpjsua-arm-none-linux-gnueabi -lpjsip-ua-arm-none-linux-gnueabi -lpjsip-simple-arm-none-linux-gnueabi -lpjsip-arm-none-linux-gnueabi -lpjmedia-codec-arm-none-linux-gnueabi -lpjmedia-arm-none-linux-gnueabi -lpjmedia-videodev-arm-none-linux-gnueabi -lpjmedia-audiodev-arm-none-linux-gnueabi -lpjmedia-arm-none-linux-gnueabi -lpjnath-arm-none-linux-gnueabi -lpjlib-util-arm-none-linux-gnueabi -lsrtp-arm-none-linux-gnueabi -lresample-arm-none-linux-gnueabi -lgsmcodec-arm-none-linux-gnueabi -lspeex-arm-none-linux-gnueabi -lilbccodec-arm-none-linux-gnueabi -lg7221codec-arm-none-linux-gnueabi -lportaudio-arm-none-linux-gnueabi -lpj-arm-none-linux-gnueabi -lm -lrt -lpthread -lsqlite3 
 endif
